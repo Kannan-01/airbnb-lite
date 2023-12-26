7284b18139341f79a2cb388433969d3873b474f0
Metalli01 <kbkannan2001@gmail.com>
2023-12-26 12:01:38 +0530

    header footer added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  brandlogo: string = './assets/images/airbnb.png';
+  userImg: string = './assets/images/People.png';
+}
