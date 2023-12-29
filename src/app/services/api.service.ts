2dd8b3bc1b45f69f01554c2fe660a892dae00835
Metalli01 <kbkannan2001@gmail.com>
2023-12-30 02:15:36 +0530

    server added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ApiService {
-  SERVER_URL = 'http://localhost:3000';
+  SERVER_URL = 'https://property-server-090y.onrender.com';
   constructor(private http: HttpClient) {}
 
   loginAPI(user: any) {
